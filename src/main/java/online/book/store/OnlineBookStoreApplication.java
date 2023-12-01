79db5e3874260e84cf159b3c0869d7d00a35b5c3
Volodymyr Ozemblovskyi <Vladimir.ozem.ps@gmail.com>
2023-12-01 21:43:04 +0200

    copy

--- src/main/java/online/book/store/OnlineBookStoreApplication.java
+++ src/main/java/online/book/store/OnlineBookStoreApplication.java
@@ -1,5 +1,10 @@
 package online.book.store;
 
+import java.math.BigDecimal;
+import java.util.List;
+import online.book.store.model.Book;
+import online.book.store.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +12,8 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
+    @Autowired
+    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
@@ -17,6 +24,15 @@ public class OnlineBookStoreApplication {
         return new CommandLineRunner() {
             @Override
             public void run(String... args) throws Exception {
+                Book testBook = new Book();
+                testBook.setTitle("testBookTitle");
+                testBook.setAuthor("testAuthor");
+                testBook.setIsbn("testIsbn");
+                testBook.setPrice(BigDecimal.valueOf(100));
+                bookService.save(testBook);
+                List<Book> books = bookService.findAll();
+                System.out.println("Print all books:");
+                books.forEach(System.out::println);
             }
         };
     }
