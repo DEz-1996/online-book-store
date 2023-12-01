55f49bf844fcb77801ef38cfc80eaf7fc87ea417
Volodymyr Ozemblovskyi <Vladimir.ozem.ps@gmail.com>
2023-12-01 16:23:08 +0200

    first commit

---
+++ src/main/java/online/book/store/OnlineBookStoreApplication.java
@@ -0,0 +1,12 @@
+package online.book.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookStoreApplication.class, args);
+    }
+}
