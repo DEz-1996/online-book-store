ce6a64018e04e2929e1009e5b4e9af101b6a4a3a
Volodymyr Ozemblovskyi <127430483+DEz-1996@users.noreply.github.com>
2023-12-05 20:31:58 +0200

    add DB, Book , BookService and BookRepository (#2)
     add DB, Book , BookService and BookRepository
    add and config work with database
    create Book model
    create and impl BookService
    create and impl BookRepository

--- src/main/java/online/book/store/OnlineBookStoreApplication.java
+++ src/main/java/online/book/store/OnlineBookStoreApplication.java
@@ -1,12 +1,22 @@
 package online.book.store;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+            }
+        };
     }
 }
