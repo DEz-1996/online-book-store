3b7589c7a2690fc5093e5cfbe70cd695008df657
Volodymyr Ozemblovskyi <Vladimir.ozem.ps@gmail.com>
2023-12-12 20:56:46 +0200

    Replaced BookRepository with use JpaRepository interface Added Liquibase support Added liquibase change file: 01-create-books-table.yaml Changed spring.jpa.hibernate.ddl-auto property value to validate Implemented soft delete concept for the Book entity Added "update" and "delete" endpoints in BookController Added "update" and "deleteById" methods for BookService

--- src/main/java/online/book/store/controller/BookController.java
+++ src/main/java/online/book/store/controller/BookController.java
@@ -5,21 +5,25 @@ import lombok.RequiredArgsConstructor;
 import online.book.store.dto.BookDto;
 import online.book.store.dto.CreateBookRequestDto;
 import online.book.store.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("/api/books")
 @RequiredArgsConstructor
 public class BookController {
 
     private final BookService bookService;
 
-    @GetMapping
+    @GetMapping()
     public List<BookDto> getAll() {
         return bookService.findAll();
     }
@@ -29,8 +33,19 @@ public class BookController {
         return bookService.getBook(id);
     }
 
-    @PostMapping
+    @PostMapping()
     public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto update(@RequestBody CreateBookRequestDto bookDto, @PathVariable Long id) {
+        return bookService.update(bookDto, id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        bookService.deleteById(id);
     }
 }
--- src/main/java/online/book/store/service/imp/BookServiceImpl.java
+++ src/main/java/online/book/store/service/imp/BookServiceImpl.java
@@ -32,8 +32,16 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
+    public BookDto update(CreateBookRequestDto bookDto, Long id) {
+        Book book = bookMapper.toBook(bookDto);
+        book.setId(id);
+        Book updatedBook = bookRepository.save(book);
+        return bookMapper.toDto(updatedBook);
+    }
+
+    @Override
     public BookDto getBook(Long id) {
-        Book book = bookRepository.findBookById(id).orElseThrow(
+        Book book = bookRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException(CANT_FIND_BY_ID_MSG + id));
         return bookMapper.toDto(book);
     }
@@ -43,5 +51,10 @@ public class BookServiceImpl implements BookService {
         return bookRepository.findAll().stream()
                 .map(bookMapper::toDto)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
     }
 }
