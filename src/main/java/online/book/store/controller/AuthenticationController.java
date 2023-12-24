59d0de6bb7ea7f7525e0aa74361e8462fe8d6c40
Volodymyr Ozemblovskyi <Vladimir.ozem.ps@gmail.com>
2023-12-24 03:49:43 +0200

    Implement JWT Security

--- src/main/java/online/book/store/controller/AuthenticationController.java
+++ src/main/java/online/book/store/controller/AuthenticationController.java
@@ -4,9 +4,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import online.book.store.dto.UserLoginRequestDto;
+import online.book.store.dto.UserLoginResponceDto;
 import online.book.store.dto.UserRegistrationRequestDto;
 import online.book.store.dto.UserResponseDto;
 import online.book.store.exception.exceptions.RegistrationException;
+import online.book.store.security.AuthenticationService;
 import online.book.store.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +22,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "User authentication", description = "Endpoints for authentication users")
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    @Operation(summary = "Login exist user", description = "Login exist user")
+    public UserLoginResponceDto login(@RequestBody UserLoginRequestDto requestDto) {
+        return authenticationService.authenticate(requestDto);
+    }
 
     @PostMapping("/registration")
     @Operation(summary = "Register new user", description = "Register new user")
